a177ace4b67d81b74e95352aebb4f8bf30b96ac3
Günther Enthaler <genthaler@users.noreply.github.com>
2026-03-12 11:25:42 +1100

    Style: enlarge robot icon in grid marker

--- src/View/Grid.elm
+++ src/View/Grid.elm
@@ -88,7 +88,7 @@ robotMarker robot =
         , spacing 4
         ]
         [ el [ centerX, Font.size 24, Font.color Theme.robotMarkerText ] (text (directionSymbol robot.facing))
-        , el [ centerX, Font.size 14, Font.color Theme.robotSubtleText ] (text "🤖")
+        , el [ centerX, Font.size 28, Font.color Theme.robotSubtleText ] (text "🤖")
         ]
 
 
