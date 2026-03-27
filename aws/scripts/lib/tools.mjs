ca1c027094c093ab683c0708ea3f975bc9a49014
Günther Enthaler <genthaler@users.noreply.github.com>
2026-03-27 21:16:01 +1100

    Remove unused AWS script helpers

--- aws/scripts/lib/tools.mjs
+++ aws/scripts/lib/tools.mjs
@@ -1,17 +1,9 @@
-import { capture, run } from "./exec.mjs";
+import { run } from "./exec.mjs";
 
 export async function npmCli(args, options = {}) {
   return run("npm", args, options);
 }
 
-export async function npmCliCapture(args, options = {}) {
-  return capture("npm", args, options);
-}
-
 export async function dockerCli(args, options = {}) {
   return run("docker", args, options);
-}
-
-export async function dockerCliCapture(args, options = {}) {
-  return capture("docker", args, options);
 }
